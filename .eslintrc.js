7574e2dbfaa13897e5fe8254615c9fbfb80ff03a
shuta13 <hollystarsun@gmail.com>
2019-10-29 19:38:36 +0900

    ,

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+"no-extra-semi": "warn",
+"no-undef": "warn",
+"no-console": "off",
+"no-multiple-empty-lines": [2, {"max": 1}],
+"no-extra-parens": 1,
+"arrow-parens": "off",
+"quotes": ["off", "single"],
+"space-before-blocks": ["warn", { "classes": "always", "functions": "always", "keywords": "always" }]
